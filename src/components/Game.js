a47c10e923db71327018dafb9b9958a49187765b
Cook1619 <cook6348@gmail.com>
2020-01-31 21:53:17 -0600

    bringing in questions from external api

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
 import Question from './Question'
 
-const dummyQuestion = {
-    question: "Whats the best programming language?",
-    answerChoices: ['JS', 'Java', 'C#', 'Fortran']
-}
-
 export default class Game extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            questions: null,
+            currentQuestion: null
+        }
+    }
+    async componentDidMount(){
+        const url = 'https://opentdb.com/api.php?amount=10'
+        try {
+            const res = await fetch(url);
+            const { results } = await res.json();
+
+            const questions = results.map((loadedQuestion) => {
+                const formattedQuestion = {
+                    question: loadedQuestion.question,
+                    answerChoices: [...loadedQuestion.incorrect_answers]
+                };
+
+                formattedQuestion.answer = Math.floor(Math.random() * 4);
+                formattedQuestion.answerChoices.splice(formattedQuestion.answer, 0, loadedQuestion.correct_answer)
+                return formattedQuestion
+            });
+            this.setState({
+                questions,
+                currentQuestion: questions[0]
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
     render() {
         return (
             <>
-                <Question question={dummyQuestion}/>
+                {this.state.currentQuestion && (
+                    <Question question={this.state.currentQuestion}/>
+                )}
+                
             </>
         )
     }
